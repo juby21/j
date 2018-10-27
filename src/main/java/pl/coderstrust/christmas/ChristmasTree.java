4a8d671080bdc8234df1f1ec69ba8aa1489205c6
piotr.baran <piotr.baran@kepak.com>
2018-10-27 20:04:31 +0100

    implement ChristmasTree

---
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -0,0 +1,31 @@
+package pl.coderstrust.christmas;
+
+public class ChristmasTree {
+    public static void main(String[] args) {
+
+        printChristmasTree(7);
+    }
+
+    public static void printChristmasTree(int size) {
+
+        int empty = size;
+        int star = 1;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= empty; j++) {   //print space
+                System.out.print(" ");
+            }
+            for (int k = 1; k < star; k++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+            empty--;
+            star += 2;
+        }
+        for (int i = 1; i < size; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
+        System.out.print("*");
+
+    }
+}

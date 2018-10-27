42e3440c4e2b0116a45a43d93c26b691367223d7
piotr.baran <piotr.baran@kepak.com>
2018-10-27 20:12:09 +0100

    implement ChristmasTree

--- src/main/java/pl/coderstrust/christmas/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -11,7 +11,7 @@ public class ChristmasTree {
         int empty = size;
         int star = 1;
         for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= empty; j++) {   //print space
+            for (int j = 1; j < empty; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k < star; k++) {
@@ -21,7 +21,7 @@ public class ChristmasTree {
             empty--;
             star += 2;
         }
-        for (int i = 1; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.print(" ");
         }
         System.out.print("*");

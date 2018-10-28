b05f3d235948546ddd262306699fdbc256d55a32
piotr.baran <piotr.baran@kepak.com>
2018-10-28 10:25:11 +0000

    IllegalArgumentException has been added

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -3,26 +3,24 @@ package pl.coderstrust.multiplication;
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        printMultiplicationTable(12);
+        printMultiplicationTable(13);
     }
 
     public static void printMultiplicationTable(int size) {
         if (size > 12) {
-            System.out.println("Maximum supported size of Multiplication Table is 12");
-        } else {
-            System.out.print("    ");
-            for (int i = 1; i <= size; i++) {
-                System.out.printf("%4d", i);
+            throw new IllegalArgumentException("Maximum supported size of Multiplication Table is 12x12");
+        }
+        System.out.print("    ");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println();
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%4d", i);
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%4d", i * j);
             }
             System.out.println();
-            for (int i = 1; i <= size; i++) {
-                System.out.printf("%4d", i);
-                for (int j = 1; j <= size; j++) {
-                    System.out.printf("%4d", i * j);
-                }
-                System.out.println();
-            }
         }
-
     }
 }

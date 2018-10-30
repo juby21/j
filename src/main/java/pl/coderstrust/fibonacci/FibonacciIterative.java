b4e98663dc5e6be4b114b2ccb1b9cf7d15d9d5ed
piotr.baran <piotr.baran@kepak.com>
2018-10-30 23:47:54 +0000

    method access modifier changed into public added throw exception

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -3,10 +3,13 @@ package pl.coderstrust.fibonacci;
 public class FibonacciIterative {
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(-1));
     }
 
-    private static long fibonacci(int fibonacciNumberInOrder) {
+    public static long fibonacci(int fibonacciNumberInOrder) {
+        if (fibonacciNumberInOrder < 1) {
+            throw new IllegalArgumentException("The Fibonacci numbers can not be less than 1 ");
+        }
         int previousNumber = 0;
         int currentNumber = 1;
         int nextNumber;
--- src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -4,14 +4,17 @@ public class FibonacciRecursive {
 
     public static void main(String[] args) {
         System.out.println(fibonacci(1));
-        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(3));
         System.out.println(fibonacci(7));
     }
 
-    private static long fibonacci(int fibonacciNumberInOrder) {
-        if (fibonacciNumberInOrder == 0) {
-            return 0;
-        } else if (fibonacciNumberInOrder == 1) {
+    public static long fibonacci(int fibonacciNumberInOrder) {
+        if (fibonacciNumberInOrder < 1) {
+            throw new IllegalArgumentException("The Fibonacci numbers can not be less than 1 ");
+        }
+        if (fibonacciNumberInOrder == 1) {
+            return 1;
+        } else if (fibonacciNumberInOrder == 2) {
             return 1;
         } else {
             return fibonacci(fibonacciNumberInOrder - 1) + fibonacci(fibonacciNumberInOrder - 2);
